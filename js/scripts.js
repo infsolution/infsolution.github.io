17dcab4cf6fbbd58bdfaed58d38f9303a266dfe6
infsolution <clsinfsolution@gmail.com>
2021-09-25 16:01:58 -0300

    add method of message with WhatsApp

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+
 /* Description: Custom JS file */
 
 
 (function($) {
     "use strict"; 
-	
+
     /* Navbar Scripts */
     // jQuery to collapse the navbar on scroll
     $(window).on('scroll load', function() {
@@ -88,5 +89,60 @@
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
 	});
+
+    /*Send Message to WhatsApp*/
+    const uri='https://message.confesta.com.br/api/'
+    const token= '$2b$10$tBSp.BGjaQhzh7ACXgh7luW_SC2dEC2hmpuOCOpyaZumZbxr20BPm'
+    const session= 'messagesite/'
+    const  point='send-message'
+    const btnSend = document.querySelector("#btnSend")
+    const name = document.querySelector("#cname")
+    const phone = document.querySelector("#cphone")
+    const cmessage = document.querySelector("#cmessage")
+    const url = `${uri}${session}${point}`
+    const message = `*Messagem da Landing Page* Olá meu nome é ${name.value} e meu número é ${phone.value} visitei seu site e desejo o seguinte: ${cmessage.value}`
+    const body = {
+        "phone": "5586988698580",
+        "message": message,
+        "isGroup": false
+      }
+      const confirme = document.querySelector("#resposta")
+    btnSend.addEventListener("click", event=>{
+        event.preventDefault()
+        if(name.value && phone.value && cmessage.value){
+            document.getElementById("btnSend").innerHTML = "Aguarde..."
+            fetch(url,{
+                method:'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                   },
+                   body:JSON.stringify(body)
+                }).then(response =>{
+                    return response.json()
+                }).then(data=>{
+                    document.getElementById("btnSend").innerHTML = "Enviar"
+                    confirme.setAttribute("class","alert alert-success")
+                    confirme.innerHTML = `Obrigado pela mensagem ${name.value}! Em breve entraremos em contato!`
+                    confirme.setAttribute("style","display:block")
+                })
+                
+        }else{
+            confirme.setAttribute("class","alert alert-danger")
+            if(!name.value){
+                confirme.innerHTML = "Por favor, informe seu nome!"
+            }else if(!phone.value){
+                confirme.innerHTML = "Por favor, informe seu telefone!"
+            }else if(!cmessage.value){
+                confirme.innerHTML = "Por favor, preencha o campo mensagem!"
+            }
+            confirme.setAttribute("style","display:block")
+        }
+
+        })
+
+        function setVisibilityButton(value){
+            btnSend.setAttribute("disabled",value)
+        }
 
 })(jQuery);
