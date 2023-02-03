d5fbe66808716bec850b3933e135717c77c5a361
infsolution <cicero.silva@w16.dev>
2023-02-03 14:52:29 -0300

    corrige token

--- pagar.php
+++ pagar.php
@@ -5,7 +5,7 @@ $ch = curl_init($baseUrl);
 $body = array("buy_order"=>"27838", "session_id"=>"sesion127838", "amount"=>50, "return_url"=>"https://https://clsdev.com.br/resposta.php");
 $options = [CURLOPT_PUT => true,
             CURLOPT_HTTPHEADER => ['Content-Type: application/json',
-                                    'Tbk-Api-Key-Secret: 579B32A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36C1C',
+                                    'Tbk-Api-Key-Secret: 01ab1a0b2edc8a7c9a5e4514ced44bfea0bfdbbdcd95899adce58c9d00c24bc3',
                                     'Tbk-Api-Key-Id:597035901884'], 
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_POSTFIELDS => json_encode($body)
